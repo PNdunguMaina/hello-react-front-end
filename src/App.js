0f54735c2dc5ab8a2ea548f0f36cae09df957d27
PNdunguMaina <ndunguwamaina@gmail.com>
2022-12-16 14:01:36 +0300

    Create all components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Greeting from './components/Greeting';
+import store from './redux/configureStore';
 
 function App() {
   return (
+    <Provider store={store}>
     <Router>
       <Routes>
         <Route path='/greetings' element={<Greeting />} />
       </Routes>
     </Router>
+    </Provider>
   );
 }
 
--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGreetings } from '../redux/greeting/greetingSlice';
 
-function Greeting(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+function Greeting() {
+  const greeting = useSelector((state) => state.reducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchGreetings());
+  }, [dispatch]);
+  return(<div>
+    <h2>Heading</h2>
+    <h1>{greeting}</h1>
+    </div>
+  )
 }
 
 export default Greeting;
